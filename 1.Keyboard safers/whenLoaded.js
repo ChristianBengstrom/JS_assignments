8162c51bc598d60693b8199351c7f9b85de55c63
ChristianBengstrom <christianbengstroem@hotmail.com>
2019-01-29 10:14:10 +0100

    *push

---
+++ 1.Keyboard safers/whenLoaded.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+ * fla12100f.js
+ * define a function to display the current time
+ */
+let theFunction = function () {
+    // code here...
+}
+
+/*
+ * activate theFunction when document has finished loading
+ */
+window.addEventListener('load', theFunction);
